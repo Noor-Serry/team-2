1ed39e504a18cfdf6244d9249a532d30ae1d62e3
noor serry <noor.serry.94@gmail.com>
2022-04-01 17:44:33 +0200

    update 1

---
+++ app/src/main/java/com/hamza/ieeechallenge/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.hamza.ieeechallenge.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hamza.ieeechallenge.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
